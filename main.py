a9a3704f0261f4e81c58e181d08784b3b1da33b7
Your Name <yagiz.algan5954@gmail.com>
2023-07-21 21:13:04 +0100

    working screen capture

---
+++ main.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+import numpy as np
+from windowcapture import WindowCapture
+
+wincap = WindowCapture("Toontown Offline")
+wincap.start()
+
+
+def apply_filter(screenshot):
+
+    return screenshot
+
+while True:
+    if wincap.screenshot is None:
+        continue
+
+    window = apply_filter(wincap.screenshot)
+    
+    cv.imshow("screen", window)
+    key = cv.waitKey(1)
+    if key == ord('q'):
+        wincap.stop()
+        cv.destroyAllWindows()
+        break
+
