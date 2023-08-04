70ea501aa743ac66c28e276b4ca5bb6e94c4eae4
Your Name <yagiz.algan5954@gmail.com>
2023-08-05 00:21:48 +0100

    small cleanup

--- minimapdetector.py
+++ minimapdetector.py
@@ -7,9 +7,6 @@ class Detector:
         self.map_white = cv.imread('filter-loopylane.png')
         self.map_white_copy = self.map_white.copy()
 
-        self.frame_count = 40
-        self.last_frames = np.array([])
-
     def main(self, filtered):
         pos, direction = self.detect_arrow(filtered)
         pos_rounded, direction_rounded = self.round(pos, direction)
@@ -19,11 +16,9 @@ class Detector:
     def detect_arrow(self, filtered):
         arrow_pixels = np.argwhere(filtered == 255)
         mean_pixel = np.mean(arrow_pixels, axis=0)
-
         centered_data = arrow_pixels - mean_pixel
 
         _, _, Vt = np.linalg.svd(centered_data, full_matrices=False)
-
         direction_estimate_raw = Vt[0][::-1]
         direction_estimate = (direction_estimate_raw / np.linalg.norm(direction_estimate_raw)) * 10
 
@@ -36,16 +31,13 @@ class Detector:
     
     def draw_circle_on_map(self, pos, direction):
         self.map_white = self.map_white_copy.copy()
-        self.map_white = self.map_white_copy.copy()
-        # Draw a circle at the specified position
-        radius = 3
-        color = (0, 255, 0)  # Green color (in BGR format)
-        thickness = -1  # Fill the circle (-1), set to a positive value for a circle outline
+
+        radius, color, thickness = 3, (0,255,0), -1
+
         cv.circle(self.map_white, pos, radius, color, thickness)
-        
+        cv.arrowedLine(self.map_white,pos, pos+direction,(255,0,0),3)
+
         print(f"Direction {direction}")
         print(f"Current position {pos}")
-        
-        cv.arrowedLine(self.map_white,pos, pos+direction,(255,0,0),3)
 
         return self.map_white
