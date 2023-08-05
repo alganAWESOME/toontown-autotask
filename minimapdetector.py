0c6e9ce01471090c926679972cfd1f5eac778fc1
Your Name <yagiz.algan5954@gmail.com>
2023-08-05 13:30:19 +0100

    Fixed 180 degree direction flipping

--- minimapdetector.py
+++ minimapdetector.py
@@ -3,17 +3,25 @@ import cv2 as cv
 
 class Detector:
     def __init__(self) -> None:
-        self.map = cv.imread('loopy-lane-map.png')
         self.map_white = cv.imread('filter-loopylane.png')
         self.map_white_copy = self.map_white.copy()
+        self.last_direction = np.array([-10,0])
 
     def main(self, filtered):
         pos, direction = self.detect_arrow(filtered)
+        self.last_direction = direction
         pos_rounded, direction_rounded = self.round(pos, direction)
         minimap = self.draw_circle_on_map(pos_rounded, direction_rounded)
         return minimap
     
     def detect_arrow(self, filtered):
+        def angle_between_vectors(v1, v2):
+            dot_product = np.dot(v1, v2)
+            magnitude_v1 = np.linalg.norm(v1)
+            magnitude_v2 = np.linalg.norm(v2)
+            angle_rad = np.arccos(dot_product / (magnitude_v1 * magnitude_v2))
+            return angle_rad
+        
         arrow_pixels = np.argwhere(filtered == 255)
         mean_pixel = np.mean(arrow_pixels, axis=0)
         centered_data = arrow_pixels - mean_pixel
@@ -21,6 +29,15 @@ class Detector:
         _, _, Vt = np.linalg.svd(centered_data, full_matrices=False)
         direction_estimate_raw = Vt[0][::-1]
         direction_estimate = (direction_estimate_raw / np.linalg.norm(direction_estimate_raw)) * 10
+
+        print(f"Last direction {self.last_direction}\nCurrent direction {direction_estimate}")
+        # if angle_between_vectors(self.last_direction, direction_estimate) > np.pi/2:
+        #     print("Switch!")
+        #     direction_estimate = -direction_estimate
+
+        if np.linalg.norm(self.last_direction + direction_estimate) < 10:
+            print("Switch!")
+            direction_estimate = -direction_estimate
 
         pos_estimate = mean_pixel[::-1]
 
