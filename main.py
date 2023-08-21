e3d3607821c1702d358332e127076d601c441b4f
Your Name <yagiz.algan5954@gmail.com>
2023-08-21 12:00:58 +0100

    Created databases for cogs and gags

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from minimap_detector import Detector
 from pathfinder import Pathfinder
 from visualizer import Visualizer, GraphCreator
 from time import sleep
+from text_detector import TextDetector
 
 def main():
     trackbars = Trackbars()
@@ -17,6 +18,7 @@ def main():
     pathfinder = Pathfinder(60)
     visualizer = Visualizer()
     graph_creator = GraphCreator(visualizer)
+    ocr = TextDetector((4, 203, 137, 23))
 
     graph_creation_mode = True
 
@@ -24,7 +26,7 @@ def main():
     while True:
         if wincap.screenshot is None:
             continue
-        
+
         filtered = trackbars.read_trackbars_and_apply_filter(wincap.screenshot)
         pos, direction = arrow_detector.main(filtered)
         minimap = visualizer.visualize(pos, direction)
@@ -32,6 +34,10 @@ def main():
             graph_creator.main(pos)
         else:
             pathfinder.main(pos, direction)
+        
+        processed_screenshot = ocr.detect_text(wincap.screenshot)
+        cv.imshow("text", processed_screenshot)
+
 
         cv.imshow("game",minimap)
         key = cv.waitKey(1)
