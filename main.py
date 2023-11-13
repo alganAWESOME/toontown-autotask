c5ce36b46efc5a0d262699dc1e27adcd76ae083c
Your Name <yagiz.algan5954@gmail.com>
2023-11-13 13:38:12 +0000

    baseline: delete everything except window capture ability

--- main.py
+++ main.py
@@ -1,44 +1,17 @@
 import cv2 as cv
-import numpy as np
 from window_capture import WindowCapture
-from filters import Trackbars
-from minimap_detector import Detector
-from pathfinder import Pathfinder
-from visualizer import Visualizer, GraphCreator
 from time import sleep
-from text_detector import TextDetector
 
 def main():
-    trackbars = Trackbars()
-
     wincap = WindowCapture("Toontown Offline")
     wincap.start()
-    
-    arrow_detector = Detector()
-    pathfinder = Pathfinder(34)
-    visualizer = Visualizer()
-    graph_creator = GraphCreator(visualizer)
-    #ocr = TextDetector((4, 203, 137, 23))
-
-    graph_creation_mode = False
 
     sleep(2)
     while True:
         if wincap.screenshot is None:
             continue
 
-        filtered = trackbars.read_trackbars_and_apply_filter(wincap.screenshot)
-        pos, direction = arrow_detector.main(filtered)
-        minimap = visualizer.visualize(pos, direction)
-        if graph_creation_mode:
-            graph_creator.main(pos)
-        else:
-            pathfinder.main(pos, direction)
-        
-        # processed_screenshot = ocr.detect_text(wincap.screenshot)
-        # cv.imshow("text", processed_screenshot)
-
-        cv.imshow("game",minimap)
+        cv.imshow("game",wincap.screenshot)
         key = cv.waitKey(1)
         if key == ord('q'):
             wincap.stop()
