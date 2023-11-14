4233c6915c9e26e03723e1e78139fa08b69ce883
Your Name <yagiz.algan5954@gmail.com>
2023-11-14 12:40:47 +0000

    created filters fot ttc_walkable and hq_door

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import cv2 as cv
 from window_capture import WindowCapture
 from time import sleep
+from filters import Filter
 
 def main():
     wincap = WindowCapture("Toontown Offline")
     wincap.start()
+    filter = Filter()
 
     sleep(2)
     while True:
         if wincap.screenshot is None:
             continue
 
+        filtered = filter.main(wincap.screenshot)
+        
         cv.imshow("game",wincap.screenshot)
+        cv.imshow('filtered', filtered)
         key = cv.waitKey(1)
         if key == ord('q'):
             wincap.stop()
